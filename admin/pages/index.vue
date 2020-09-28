8935f6c5c8ca821f029728730e4a890a55c50f43
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-09-28 15:37:33 -0700

    Update index page to delete and navigate

--- admin/pages/index.vue
+++ admin/pages/index.vue
@@ -7,14 +7,17 @@
           <h1 class="a-size-large a-spacing-none a-text-normal">
             All Products
           </h1>
+          <div class="a-spacing-top-medium"></div>
 
           <!-- Buttons -->
-          <a href="#" class="a-button-buy-again">Add a new product</a>
-          <a href="#" class="a-button-history margin-right-10"
-            >Add a new category</a
+          <nuxt-link to="/products" class="a-button-buy-again margin-right-10"
+            >Add a new product</nuxt-link
           >
-          <a href="#" class="a-button-history margin-right-10"
-            >Add a new owner</a
+          <nuxt-link to="/category" class="a-button-history margin-right-10"
+            >Add a new category</nuxt-link
+          >
+          <nuxt-link to="/owner" class="a-button-history margin-right-10"
+            >Add a new owner</nuxt-link
           >
         </div>
       </div>
@@ -25,7 +28,7 @@
     <div class="container-fluid browsing-history">
       <div class="row">
         <div
-          v-for="product in products"
+          v-for="(product, index) in products"
           :key="product.id"
           class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb"
         >
@@ -64,8 +67,17 @@
 
             <!-- Product Buttons -->
             <div class="a-row">
-              <a href="#" class="a-button-history margin-right-10">Update</a>
-              <a href="#" class="a-button-history margin-right-10">Delete</a>
+              <nuxt-link
+                :to="`/products/${product._id}`"
+                class="a-button-history margin-right-10"
+                >Update</nuxt-link
+              >
+              <a
+                to="#"
+                class="a-button-history margin-right-10"
+                @click="onDeleteProduct(product._id, index)"
+                >Delete</a
+              >
             </div>
           </div>
         </div>
@@ -83,6 +95,19 @@ export default {
         products: response.products
       };
     } catch (err) {}
+  },
+
+  methods: {
+    async onDeleteProduct(id, index) {
+      try {
+        let response = await this.$axios.$delete(
+          `http://localhost:3000/api/products/${id}`
+        );
+        this.products.splice(index, 1);
+      } catch (err) {
+        console.log(err.response.data.message);
+      }
+    }
   }
 };
 </script>
