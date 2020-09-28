0e9d90ab57b091235e9a34ef49e080d664810a78
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-09-27 23:25:20 -0700

    Fix upload issues

--- server/middlewares/upload-photo.js
+++ server/middlewares/upload-photo.js
@@ -12,10 +12,10 @@ const s3 = new aws.S3();
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: "amazon_web_clone_bucket",
-    acl: "public-read",
+    bucket: "amazon-web-clone-bucket",
+    acl: "private",
     metadata: (req, file, cb) => {
-      cb(null, { fieldName: file.fieldName });
+      cb(null, { fieldname: file.fieldname });
     },
     key: (req, file, cb) => {
       cb(null, Date.now().toString());
