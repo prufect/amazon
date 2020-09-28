f115410c78179227d8d7b3786307349347a6002f
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-09-27 22:50:01 -0700

    update server to use products router

--- server/server.js
+++ server/server.js
@@ -20,6 +20,8 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const productRoutes = require("./routes/product");
+app.use("/api", productRoutes);
 
 app.listen(3000, (err) => {
   if (err) console.log(err);
