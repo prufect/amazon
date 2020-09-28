b029eb7e7d62f63ec694376e9a25742318a24297
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-09-27 22:49:49 -0700

    Create products router

---
+++ server/routes/product.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const Product = require("../models/product");
+
+router.post("/products", async (req, res) => {
+  try {
+    let product = new Product();
+    product.title = req.body.title;
+    product.description = req.body.description;
+    product.photo = req.body.photo;
+    product.stockQuantity = req.body.stockQuantity;
+    await product.save();
+
+    res.json({
+      status: true,
+      message: "success",
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+module.exports = router;
