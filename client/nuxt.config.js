8f2916b74a4cdd02605e3c29388e5b301201d915
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-09-29 14:27:06 -0700

    Update client app to use axios

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -29,8 +29,13 @@ export default {
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    "bootstrap-vue/nuxt"
+    "bootstrap-vue/nuxt",
+    "@nuxtjs/axios"
   ],
+
+  axios: {
+    baseURL: "http://localhost:3000"
+  },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {}
