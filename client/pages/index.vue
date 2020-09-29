0692d3a46aad6888236ed1bc4fa4195edb8b7598
Prudhvi <gadiraju.prudhvi@gmail.com>
2020-09-29 13:03:05 -0700

    Add featured product

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,34 +1,24 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        client
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <main class="listingPage">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xl-2 col-lg-3 md-4 col-sm-4"></div>
+
+        <div class="col-xl-10 col-lg-9 md-8 col-sm-8">
+          <FeaturedProduct />
+        </div>
       </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <script>
-export default {};
+import FeaturedProduct from "~/components/FeaturedProduct";
+export default {
+  components: {
+    FeaturedProduct
+  }
+};
 </script>
 
 <style></style>
